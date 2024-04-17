9adf8288d6b046d85c6df190f3083ccd14e1d1e8
Arif Paiman <arifpaiman@Arifs-MacBook-Pro.local>
2024-04-17 15:52:35 -0500

    added

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import post
 
-admin.site.register(post)
+class postadmin(admin.ModelAdmin):
+    list_display = ('title','is_enabled','published_date','created_date','updated_date')
+
+admin.site.register(post,postadmin)
 # Register your models here.
--- posts/models.py
+++ posts/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class post(models.Model):
     title= models.CharField(max_length=10)
-    text= models.CharField(max_length=50)
+    text= models.TextField(max_length=500, blank=True)
     is_enabled= models.BooleanField(default=True)
     published_date = models.DateField()
     created_date = models.DateField()
-    updated_date = models.DateTimeField()
+    updated_date = models.DateTimeField(auto_now=False)
 
 # Create your models here.
